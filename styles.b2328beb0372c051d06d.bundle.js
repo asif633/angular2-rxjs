9a7d4d4caf98bb0b68012bff407ed6143f95ad94
Asif Iqubal <asif633@gmail.com>
2016-12-22 02:34:54 +0530

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{330:function(n,o){},599:function(n,o,c){n.exports=c(330)}},[599]);
+webpackJsonp([1,2],{389:function(n,o){},924:function(n,o,c){n.exports=c(389)}},[924]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
